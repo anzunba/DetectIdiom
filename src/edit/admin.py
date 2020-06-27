bdf80e00f01d280c9e89f44b9d02fe0d114af483
anzunba <nakatsuji.anna@gmail.com>
2020-06-27 11:18:27 -1000

    Created models

--- src/edit/admin.py
+++ src/edit/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(EnJa)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'content', 'from_lang', 'to_lang')
+    
+class WordIdiomTableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'article', 'user', 'word', 'idiom', 'created_at')
+    
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bio', 'profile_img', 'native_lang', 'created_at')
+
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'followed_user')
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'article', 'user', 'is_favorite', 'is_like', 'is_comment', 'is_reply', 'is_reply_like', 'is_following', 'created_at')
+    
+class NativeLanguageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'lang', 'level')
+
+class LanguageLevelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'level')
+
+class ArticleLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'article', 'user')
+    
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'article', 'user')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'article', 'user', 'content', 'created_at')
+
+class CommentLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment', 'user')
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment', 'user', 'content', 'created_at')
+    
+class ReplyLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reply', 'user')
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(WordIdiomTable, WordIdiomTableAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Following, FollowingAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(NativeLanguage, NativeLanguageAdmin)
+admin.site.register(LanguageLevel, LanguageLevelAdmin)
+admin.site.register(ArticleLike, ArticleLikeAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(CommentLike, CommentLikeAdmin)
+admin.site.register(Reply, ReplyAdmin)
+admin.site.register(ReplyLike, ReplyLikeAdmin)
--- src/edit/models.py
+++ src/edit/models.py
@@ -7,9 +7,91 @@ import os
 
 # Create your models here.
 class EnJa(models.Model):
-  word = models.CharField(max_length=100)
-  mean = models.CharField(max_length=2000)
+  word = models.CharField(max_length=63)
+  mean = models.CharField(max_length=511)
 
   def __str__(self):
     return self.word
+  
+class Article(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=63)
+  content = models.TextField()
+  from_lang = models.ForeignKey('Language', on_delete=models.CASCADE, related_name = "from_lang")
+  to_lang = models.ForeignKey('Language', on_delete=models.CASCADE, related_name = "to_lang")
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class WordIdiomTable(models.Model):
+  article = models.ForeignKey('Article', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  word = models.TextField()
+  idiom = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = models.TextField(max_length=255, blank=True)
+  profile_img = models.ImageField(upload_to='images/profile/')
+  native_lang = models.ForeignKey('Language', on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class Following(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "following_user")
+  followed_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "followed_user")
+
+class Notification(models.Model):
+  article = models.ForeignKey('Article', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  is_favorite = models.BooleanField(default=False)
+  is_like = models.BooleanField(default=False)
+  is_comment = models.BooleanField(default=False)
+  is_comment_like = models.BooleanField(default=False)
+  is_reply = models.BooleanField(default=False)
+  is_reply_like = models.BooleanField(default=False)
+  is_following = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  
+class NativeLanguage(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  lang = models.ForeignKey('Language', on_delete=models.CASCADE)
+  level = models.ForeignKey('LanguageLevel', on_delete=models.CASCADE)
+
+class LanguageLevel(models.Model):
+  level = models.CharField(max_length=15)
+
+class Language(models.Model):
+  lang_name = models.CharField(max_length=15)
+
+class ArticleLike(models.Model):
+  article = models.ForeignKey('Article', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Favorite(models.Model):
+  article = models.ForeignKey('Article', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+  article = models.ForeignKey('Article', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  content = models.CharField(max_length=255, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class CommentLike(models.Model):
+  comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Reply(models.Model):
+  comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  content = models.CharField(max_length=255, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class ReplyLike(models.Model):
+  reply = models.ForeignKey('Reply', on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  
+
+  
   
